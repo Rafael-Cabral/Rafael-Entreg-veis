738a97315d209542c9c8118a434fc1a4b40041dc
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2022-06-21 15:50:33 -0300

    Ex 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/ex1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/ex1.js
@@ -32,3 +32,57 @@ function nomes(){
     nomes = nomes.sort();
     document.getElementById("resultado2").innerHTML = nomes
 }
+
+
+function sequencia(){
+    var n = document.getElementById("n1").value;
+    n = parseInt(n);
+    var cont = 1;
+    var n1 = 0;
+
+    if (n == 1){
+        document.getElementById("resultado3").innerHTML = "A sequência é: 1";
+    }
+    if (n == 2){
+        document.getElementById("resultado3").innerHTML = "A sequência é: 1, 1, ";
+    }
+    if (n == 3){
+        document.getElementById("resultado3").innerHTML = "A sequência é: 1, 1, 2";
+    }else{
+        final2 = [];
+        var final = 2;
+        while (cont < (n-2)){
+            final = final+ cont;
+            final2.push(final);
+            cont++
+        };
+        document.getElementById("show").innerHTML = "a sequência é: 1, 1, 2, " + final2;
+    }
+
+}
+
+function primo(){
+    debugger
+    var min = Number(document.getElementById("min").value)
+    var max = Number(document.getElementById("max").value)
+    var lista = [];
+
+    if (min > 0){
+        lista = [1, 2]
+    }
+
+    for(let i = min; i <= max; i++){
+        if (i > 2){
+            var cont = 0;
+            for (let index = 2; index < i; index++){
+                if (i % index == 0){
+                    cont++
+                }
+            }
+            if(cont == 0){
+                lista.push(i);
+            }
+        }
+    }
+    document.getElementById("show1").innerHTML = lista;
+}
